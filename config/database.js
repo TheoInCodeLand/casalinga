d3ff90c3bc1394e969a5182bda6927924680eef0
Theophilus <152610462+TheoInCodeLand@users.noreply.github.com>
2026-01-25 17:09:29 +0200

    Change database connection string to use DATABASE_URL

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL_TEST,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
