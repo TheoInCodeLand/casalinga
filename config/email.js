ce6f13ed98c997bb328f3b6efd39c9ce2cedfa72
Africonnect Tech Solutions <thobejanetheo@gmail.com>
2026-01-25 20:44:12 +0200

    email config

--- config/email.js
+++ config/email.js
@@ -4,8 +4,8 @@ require('dotenv').config();
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        user: 'thobejanetheo@gmail.com',
+        pass: 'ypue gyqz szbu vjen',
     },
     connectionTimeout: 10000, 
     greetingTimeout: 10000,
